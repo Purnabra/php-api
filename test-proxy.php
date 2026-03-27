ae3210f99c15ee4f8c5d006761be43e3584c3764
purnabrata <mukherjeepurnabrata1998@gmail.com>
2026-03-27 16:12:18 +0530

    added successfully

---
+++ test-proxy.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "hello how r you";
+/*
+$url='https://sangharileuk.ct.ws';//api endpoint url
+$ch=curl_init();
+curl_setopt_array($ch,[
+CURLOPT_URL=>$url,
+CURLOPT_RETURNTRANSFER=>TRUE,
+CURLOPT_FOLLOWLOCATION=>TRUE,
+CURLOPT_SSL_VERIFYPEER=>FALSE,
+//CURLOPT_HEADERFUNCTION=>$header_callback
+
+CURLOPT_HEADER=>false
+]);
+
+$result=curl_exec($ch);
+echo $result;
+
+
+curl_close($ch);
+
+*/
+?>
