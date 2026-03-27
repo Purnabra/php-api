d182675034073c9d56347055bc1cf7281a30ea11
purnabrata <mukherjeepurnabrata1998@gmail.com>
2026-03-27 16:01:27 +0530

    chnages something

--- api-response.php
+++ api-response.php
@@ -1,25 +1,21 @@
 <?php
-header('Content-Type: application/json');
-//echo "hello world","how r you";
-
-//print_r($_SERVER);
-//echo substr($_SERVER['REQUEST_URI'],1);
-
-$r=parse_url(substr($_SERVER['REQUEST_URI'],1),PHP_URL_PATH);;
-
+$r=parse_url(substr($_SERVER['REQUEST_URI'],1),PHP_URL_PATH);
+$array_page=explode('/',$r);
+if(count($array_page)<=2):
+http_response_code(404);
+exit;
+else:
 list($root_folder,$page,$request_id)=explode('/',$r);	//print_r($r);
+endif;
 
-
-//echo $page,'==',$request_id;
-
-
-if($page!='api')
+if($page!='api' || count($array_page)>3 )
 {
 
 http_response_code(404);
 exit;
 
 }
+
 $url='https://randomuser.me/api';//api endpoint url
 /*$reponse_headers=array();
 $header_callback=function($ch1,$header) use(&$reponse_headers){
@@ -34,6 +30,10 @@ $reponse_headers[$parts[0]]=trim($parts[1]);
 
 return strlen($header);
 };*/
+
+
+
+header('Content-Type: application/json');
 $ch=curl_init();
 curl_setopt_array($ch,[
 CURLOPT_URL=>$url,
@@ -56,4 +56,6 @@ curl_close($ch);
 
 //echo "hello world";
 //print_r($path)
+
+
 ?>
